6e7d4bbc4376b34d540ad04ccf83e6523045785f
DESKTOP-CFOMFKS\User <marius.pasiulevicius@gmail.com>
2021-06-11 20:28:20 +0300

    cart css added

--- components/product/productItem.js
+++ components/product/productItem.js
@@ -1,6 +1,13 @@
-const ProductItem = () =>{
+const ProductItem = ({product}) =>{
     return(
-        <div>ProductItem</div>
+        <div className="card" style={{width: "18rem"}}>
+            <img src={product.images[0].url} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
     )
 };
 
