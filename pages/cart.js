52378900a44e8f3f926b86da4c76d830abdaf1cf
DESKTOP-CFOMFKS\User <marius.pasiulevicius@gmail.com>
2021-06-17 00:23:20 +0300

    small fix for production

--- pages/cart.js
+++ pages/cart.js
@@ -4,7 +4,7 @@ import { DataContext } from '../store/GlobalState';
 import CartItem from '../components/CartItem';
 import Link from 'next/link'
 import { getData } from '../utils/fetchData';
-import PaypalBtn from './paypallBtn';
+import PaypalBtn from '../components/paypallBtn';
 
 const Cart = () =>{
     const {state, dispatch} = useContext(DataContext)
